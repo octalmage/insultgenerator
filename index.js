8532a494c9f89e3f1d05a24832e545471672b277
Jason Stallings <jason@stallin.gs>
2015-10-01 20:51:40 -0500

    Decode HTML entities.

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ module.exports = function (callback)
 		{
 			var start = body.substring(body.indexOf("<br><br>") + 8);
 			var insult = start.substring(0, start.indexOf("</div>"));
+
+			//Fix random HTML entities.
+			insult = insult.replace(/&nbsp;/g, " "); //Replace "&nbsp;".
+			insult = insult.replace(/&#44;/g, ","); //Replace "&#44;".
+			insult = insult.replace(/&amp;/g, "&"); //Replace "&amp;".
+			insult = insult.replace(/&lt;/g, "<"); //Replace "&lt;".
+			insult = insult.replace(/&gt;/g, ">"); //Replace "&gt;".
+			insult = insult.replace(/&quot;/g, "\""); //Replace "&quot;".
+			insult = insult.replace(/&#96;/g, "`"); //Replace "&#96;".
+			insult = insult.replace(/&#x27;/g, "'"); //Replace "&#x27;".
+			insult = insult.replace(/\n/g, ""); //Remove newlines.
+
 			callback(insult);
 		});
 	});
